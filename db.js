41c955b771bdc30658945ed57c9ef6efe458190a
celsojunio11 <celsojunio11@gmail.com>
2021-05-20 20:28:04 -0300

    Atualizar - 2 Aula

--- db.js
+++ db.js
@@ -2,20 +2,32 @@ var conexao = require("mongodb").MongoClient;
 var idEmpresa = require("mongodb").ObjectId;
 
 conexao.connect("mongodb://localhost/empresa")
-  .then(conn => global.conn = conn.db("empresa"))
-  .catch(error => console.log(error))
+    .then(conn => global.conn = conn.db("empresa"))
+    .catch(error => console.log(error))
 
-//listar empresas
 function listarEmpresas(callback) {
-  global.conn.collection("dadosempresa").find({}).sort({nome: 1}).toArray(callback)
+    global.conn.collection("dadosempresa").find({}).sort({ nome: 1 }).toArray(callback);
 }
 
-//função inserir
 function inserirEmpresa(docEmpresa, callback) {
-  global.conn.collection("dadosempresa").insert(docEmpresa, callback);
+    global.conn.collection("dadosempresa").insert(docEmpresa, callback);
 }
 
 function buscarPorId(id, callback) {
-  global.conn.collection("dadosempresa").find(new idEmpresa(id)).toArray(callback);
+    global.conn.collection("dadosempresa").find(new idEmpresa(id)).toArray(callback);
 }
-module.exports = { listarEmpresas, inserirEmpresa, buscarPorId };
+
+function atualizarEmpresa(id, docEmpresa, callback) {
+    global.conn.collection("dadosempresa")
+        .updateOne({ _id: new idEmpresa(id) },
+            {
+                $set: {
+                    nome: docEmpresa.nome,
+                    numero: docEmpresa.numero,
+                    razaoSocial: docEmpresa.razaoSocial
+                }
+            }, callback);
+}
+
+
+module.exports = { listarEmpresas, inserirEmpresa, buscarPorId, atualizarEmpresa };
--- routes/index.js
+++ routes/index.js
@@ -2,49 +2,63 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
   global.db.listarEmpresas((error, docs) => {
-    if (error) {
+    if(error){
       return console.log(error);
     }
-    res.render('index', { title: 'Lista de Empresas', dadosempresa: docs });
+    res.render('index', { title: 'Lista de Empresas', dadosempresa : docs});
   })
-
 });
 
-/* GET novo cadastro */
-router.get('/new', function (req, res, next) {
-  res.render('new', { title: 'Formulario de Cadastro',
-                    dadosempresa: {"nome": "", "numero":""},
-                    action: '/new' });
+/* GET novo cadastro. */
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'Formulário de Cadastro',
+                      dadosempresa: {"nome":"", "numero":"", "razaoSocial":""}, 
+                      action: '/new'});
 });
 
-/* POST nova empresa */
-
-router.post('/new', function (req, res, next) {
+/*POST new empresa */
+router.post('/new', function(req, res, next){
   var nome = req.body.nome;
-  var numero = req.body.numero;
-  global.db.inserirEmpresa({ nome, numero }, (error, result) => {
-    if (error) {
+  var numero = parseInt(req.body.numero);
+  var razaoSocial = req.body.razaoSocial;
+
+  global.db.inserirEmpresa({nome, numero, razaoSocial}, (error, result)=>{
+    if(error){
+      return console.log(error);}
+      res.redirect('/');    
+  })
+});
+
+/*ROTA GET retornar as informações da empresa*/
+router.get('/edit/:id', function(req, res){
+  var id = req.params.id;
+
+  global.db.buscarPorId(id, (error, docs)=>{
+    if(error){
+      return console.log(error);
+    }
+    res.render('new', {title: 'Editar Empresa', dadosempresa: docs[0],
+                                                action: '/edit/' + docs[0]._id});
+    })
+});
+
+
+/* rota post EDIT*/
+router.post('/edit/:id', function(req, res){
+  var id = req.params.id;
+  var nome = req.body.nome;
+  var numero = parseInt(req.body.numero);
+  var razaoSocial = req.body.razaoSocial;
+
+  global.db.atualizarEmpresa(id, {nome, numero, razaoSocial}, (error, result) => {
+    if(error){
       return console.log(error);
     }
     res.redirect('/');
   })
-});
-
-/*ROTA GET retornar as informaçoes da empresa*/
-
-router.get('/edit/:id', function (req, res) {
-  var id = req.params.id;
-
-  global.db.buscarPorId(id, (error, docs) => {
-    if (error) {
-      return console.log(error);
-    }
-    res.render('new', {title: 'Editar Empresa', dadosempresa: docs[0], action: '/edit'+ docs[0]._id});
-  })
-
-});
+})
 
 
 module.exports = router;
